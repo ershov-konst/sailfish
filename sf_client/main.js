252a58666c37b6c690eb6f81e57924797ed6357c
Ershov Konstantin <ershov.konst@gmail.com>
2013-07-24 17:16:02 +0400

    добавлен плагин is для requirejs

--- sf_client/main.js
+++ sf_client/main.js
@@ -7,6 +7,9 @@ requirejs.config({
       "css-normalize" : "sf_client/ext/requirejs/plugins/css-normalize",
       "css-builder"   : "sf_client/ext/requirejs/plugins/css-builder",
       "doT"           : "sf_client/ext/dot/doT",
-      "text"          : "sf_client/ext/requirejs/plugins/text"
+      "text"          : "sf_client/ext/requirejs/plugins/text",
+      "is"            : "sf_client/ext/requirejs/plugins/is",
+      "is-api"        : "sf_client/ext/requirejs/plugins/is-api",
+      "is-builder"    : "sf_client/ext/requirejs/plugins/is-builder"
    }
 });
