e2c46a13e986b678bcf97c9cc39a3f7181d4104a
ershov-konst <ershov.konst@gmail.com>
2013-12-11 21:44:20 +0400

    minor refactoring - renamed arguments

--- sf_client/lib/EventBus.js
+++ sf_client/lib/EventBus.js
@@ -5,42 +5,42 @@ define("js!EventBus", function(){
    };
 
    EventBusClass.prototype = {
-      on :function(type, callback, scope) {
+      on :function(eventName, callback, scope) {
          var args = [];
          var numOfArgs = arguments.length;
          for(var i=0; i<numOfArgs; i++){
             args.push(arguments[i]);
          }
          args = args.length > 3 ? args.splice(3, args.length-1) : [];
-         if(typeof this.listeners[type] != "undefined") {
-            this.listeners[type].push({scope:scope, callback:callback, args:args});
+         if(typeof this.listeners[eventName] != "undefined") {
+            this.listeners[eventName].push({scope:scope, callback:callback, args:args});
          } else {
-            this.listeners[type] = [{scope:scope, callback:callback, args:args}];
+            this.listeners[eventName] = [{scope:scope, callback:callback, args:args}];
          }
       },
-      off :function(type, callback, scope) {
-         if(typeof this.listeners[type] != "undefined") {
-            var numOfCallbacks = this.listeners[type].length;
+      off :function(eventName, callback, scope) {
+         if(typeof this.listeners[eventName] != "undefined") {
+            var numOfCallbacks = this.listeners[eventName].length;
             var newArray = [];
             for(var i=0; i<numOfCallbacks; i++) {
-               var listener = this.listeners[type][i];
+               var listener = this.listeners[eventName][i];
                if(listener.scope == scope && listener.callback == callback) {
 
                } else {
                   newArray.push(listener);
                }
             }
-            this.listeners[type] = newArray;
+            this.listeners[eventName] = newArray;
          }
       },
-      has :function(type, callback, scope) {
-         if(typeof this.listeners[type] != "undefined") {
-            var numOfCallbacks = this.listeners[type].length;
+      has :function(eventName, callback, scope) {
+         if(typeof this.listeners[eventName] != "undefined") {
+            var numOfCallbacks = this.listeners[eventName].length;
             if(callback === undefined && scope === undefined){
                return numOfCallbacks > 0;
             }
             for(var i=0; i<numOfCallbacks; i++) {
-               var listener = this.listeners[type][i];
+               var listener = this.listeners[eventName][i];
                if(listener.scope == scope && listener.callback == callback) {
                   return true;
                }
@@ -48,11 +48,11 @@ define("js!EventBus", function(){
          }
          return false;
       },
-      trigger :function(type, target) {
+      trigger :function(eventName, data) {
          var numOfListeners = 0;
          var event = {
-            type:type,
-            target:target
+            type:eventName,
+            target:data
          };
          var args = [];
          var numOfArgs = arguments.length;
@@ -61,10 +61,10 @@ define("js!EventBus", function(){
          }
          args = args.length > 2 ? args.splice(2, args.length-1) : [];
          args = [event].concat(args);
-         if(typeof this.listeners[type] != "undefined") {
-            var numOfCallbacks = this.listeners[type].length;
+         if(typeof this.listeners[eventName] != "undefined") {
+            var numOfCallbacks = this.listeners[eventName].length;
             for(var j=0; j<numOfCallbacks; j++) {
-               var listener = this.listeners[type][j];
+               var listener = this.listeners[eventName][j];
                if(listener && listener.callback) {
                   var concatArgs = args.concat(listener.args);
                   listener.callback.apply(listener.scope, concatArgs);
