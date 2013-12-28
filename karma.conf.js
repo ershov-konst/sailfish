8a808843d4ca166bb59b4b05497c4ef86461c8a8
ershov-konst <ershov.konst@gmail.com>
2013-12-28 14:08:51 +0400

    update karma dependency

--- karma.conf.js
+++ karma.conf.js
@@ -1,69 +1,70 @@
 // Karma configuration
-// Generated on Sat Dec 28 2013 13:26:55 GMT+0400 (MSK)
+// Generated on Sat Dec 28 2013 14:08:07 GMT+0400 (MSK)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
-  {pattern: 'sf_client/**/*.js', included: false},
-  {pattern: 'test/**/*Spec.js', included: false},
-  'test/test-main.js'
-];
-
-// list of files to exclude
-exclude = [
-  
-];
+    // list of files / patterns to load in the browser
+    files: [
+      {pattern: 'sf_client/**/*.js', included: false},
+      {pattern: 'test/**/*Spec.js', included: false},
+      'test/test-main.js'
+    ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// web server port
-port = 9876;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// cli runner port
-runnerPort = 9100;
+    // web server port
+    port: 9876,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
